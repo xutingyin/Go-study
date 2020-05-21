7c38d167baf95c682c9fe7cc206f79b49d3546a7
xuty <sunshinexuty@163.com>
2020-05-21 16:32:17 +0800

    feat:修改目录

--- src/Hello.go
+++ src/Hello.go
@@ -1,6 +1,9 @@
 package main // 包声明
 
-import "fmt" // 引入 fmt 格式化IO 的包
+import (
+	"fmt"
+	"time"
+) // 引入 fmt 格式化IO 的包
 
 func main() { //定义main函数，每个程序的入口；另外 { 不能单独一行
 	/*
@@ -8,4 +11,5 @@ func main() { //定义main函数，每个程序的入口；另外 { 不能单独
 		也称为块注释
 	*/
 	fmt.Println("Let's Go!") // ①Println 函数名的首字母必须大写；②每一行命令最后的分号不许手动写，Go 编辑器会自动加上
+	time.Sleep(time.Duration(3 * time.Second))
 }
